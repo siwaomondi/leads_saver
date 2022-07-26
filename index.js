4165ccaf4650e6200564c1feaf6ad31592b59874
Smtp272 <101472734+Smtp272@users.noreply.github.com>
2022-07-26 20:08:12 +0300

    Add email share and clipboard copy functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const selectAllBtn = document.getElementById("sel-all-btn")
 const deselectAllBtn = document.getElementById("deselect-btn")
 const selectInverseBtn = document.getElementById("inverse-sel-btn")
 const deleteSelectionBtn = document.getElementById("delete-sel-btn")
+const copySelectionBtn = document.getElementById("copy-sel-btn")
 const sendSelectionBtn = document.getElementById("send-btn")
 const leadP = document.getElementById("list-of-leads")
 const leadsFromLocal = JSON.parse(localStorage.getItem('myLeads'))
@@ -22,7 +23,7 @@ if (leadsFromLocal) {
 contactsEL.innerHTML = `<p>©RavelTech ${year}</p>`
 
 tabBtn.addEventListener('click', function () {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         let url = tabs[0].url
         let row = findRow(url)
         if (alreadyExistsCheck(url)) {
@@ -148,67 +149,107 @@ function selection(filter) {
 
 
 }
+copySelectionBtn.addEventListener("click", () => {
+    let listOfUrls = checkLinks()
+    let text = ''
+    for(let i=0;i<listOfUrls.length;i++){
+        text += `-  ${listOfUrls[i]}\n`
+    }
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Links successfully copied to clipboard");
+    }).catch(() => {
+        alert("something went wrong");
+    });
+})
+
 
 sendSelectionBtn.addEventListener("click", function () {
-    let input, listOfUrls, userEmailAddress, formattedMessage
+    let input, listOfUrls, formattedMessage, recepient_email_address, subject
     listOfUrls = checkLinks()
-
-    function createEmailBody() {
-        let message = 'Here you go:'
-        for (let i = 0; i < listOfUrls.length; i++) {
-            message += `\n    - ${listOfUrls[i]}`
-        }
-        return message
-    }
+    formattedMessage = createEmailBody(listOfUrls)
 
     if (listOfUrls.length > 0) {
-        let userPassword = "*********"
-        formattedMessage = createEmailBody()
-        userEmailAddress = prompt("Please enter email Address","johndoe@email.com")
-        if (validateEmail(userEmailAddress)) {
-            console.log(listOfUrls)
-            console.log(formattedMessage)
-            sendEmail(sender_email_address=userEmailAddress, password=userPassword, recepient_email_address=userEmailAddress, message=formattedMessage)
-        } else {
-            alert('invalid Email Address')
+        if (listOfUrls.length >= 3) alert('Can only send two links at a time')
+        else {
+            let subject = `Links from Leads_saver`
+            recepient_email_address = prompt("Please enter recepient email Address")
+            if (validateEmail(recepient_email_address)) {
+                let link = `mailto:${recepient_email_address}?&subject=${subject}&body=${formattedMessage}`
+                document.getElementById("send-link-anchor").href = link
+            } else {
+                alert('Invalid Email Address')
+            }
         }
     } else {
         alert("No links selected")
     }
 
-    function checkLinks() {
-        let i, list, allCheckboxes
-        list = []
-        allCheckboxes = document.getElementsByName("rowCheckbox")
-        for (i = 0; i < allCheckboxes.length; i++) {
-            input = allCheckboxes[i]
-            if (input.checked) {
-                list.push(input.dataset.rowUrl)
-            }
-        }
-        return list
-    }
-
 })
 
+function checkLinks() {
+    let i, list, allCheckboxes
+    list = []
+    allCheckboxes = document.getElementsByName("rowCheckbox")
+    for (i = 0; i < allCheckboxes.length; i++) {
+        input = allCheckboxes[i]
+        if (input.checked) {
+            list.push(input.dataset.rowUrl)
+        }
+    }
+    return list
+}
+
+function createEmailBody(urls) {
+    let message = "Here are your links:%0A"
+    for (let i = 0; i < urls.length; i++) {
+        message += `%0A    -${urls[i]}`
+    }
+    let closingTag = `%0A%0AWelcome%2C%0A%0ARavelTech.`
+    message += closingTag
+    console.log(message)
+    return message
+}
+
 function validateEmail(emailAddress) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress) && emailAddress !== "johndoe@email.com") {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailAddress)) {
         return true
     } else {
         alert("Invalid email address!")
     }
 }
 
-function sendEmail(sender_email_address, password, recepient_email_address, message) {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: `${sender_email_address}`,
-        Password: `${password}`,
-        To: `${recepient_email_address}`,
-        From: `${sender_email_address}`,
-        Subject: "Links from RavelTech",
-        Body: `${message}`,
-    }).then(
-        message => alert("mail sent successfully")
-    )
-}
+// sendSelectionBtn.addEventListener("click", function () {
+//     let input, listOfUrls, userEmailAddress, formattedMessage
+//     listOfUrls = checkLinks()
+
+//     if (listOfUrls.length > 0) {
+//         let userPassword = "*********"
+//         formattedMessage = createEmailBody(listOfUrls)
+//         userEmailAddress = prompt("Please enter email Address", "johndoe@email.com")
+//         if (validateEmail(userEmailAddress)) {
+//             sendEmail(sender_email_address = userEmailAddress, password = userPassword, recepient_email_address = userEmailAddress, message = formattedMessage)
+//         } else {
+//             alert('invalid Email Address')
+//         }
+//     } else {
+//         alert("No links selected")
+//     }
+
+// })
+
+// function sendEmail(sender_email_address, password, recepient_email_address, message) {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: `${sender_email_address}`,
+//         Password: `${password}`,
+//         To: `${recepient_email_address}`,
+//         From: `${sender_email_address}`,
+//         Subject: "Links from RavelTech",
+//         Body: `${message}`,
+//     }).then(
+//         message => alert("mail sent successfully")
+//     )
+// }
+
+
+
